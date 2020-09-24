76b988d2bb511cc4a422b1e3bf04d5c23bd5ef62
Jorge <jorgeparraandrade@gmail.com>
2020-09-24 15:25:59 -0400

    Responsive and desktop menu

--- parts/global/site-header.php
+++ parts/global/site-header.php
@@ -10,7 +10,7 @@ $logo = wp_get_attachment_image_src(get_field('header_logo', 'option'), 'logo');
 <header class="site-header">
   <div class="container">
     <div class="row">
-      <div class="col-12">
+      <div class="col-12 fix">
 
         <?php // Logo ?>
         <a href="<?php echo esc_url(home_url()); ?>" class="site-header__logo">
@@ -20,16 +20,19 @@ $logo = wp_get_attachment_image_src(get_field('header_logo', 'option'), 'logo');
         <h1>September 30 — October 2</h1>
 
         <?php // Primary menu
-        wp_nav_menu(
-          array(
-            'theme_location' => 'primary',
-            'container' => 'nav',
-            'container_class' => 'nav--primary lg-only',
-          )
-        ); ?>
+          // wp_nav_menu(
+          //   array(
+          //     'theme_location' => 'primary',
+          //     'container' => 'nav',
+          //     'container_class' => 'nav--primary lg-only',
+          //   )
+          // );
+        ?>
 
         <?php // Mobile nav button ?>
-        <button class="menu-icon"><span></span></button>
+          <div class="menu-icon" >
+            <a id="nifty-nav-toggle" class="nifty-hamburger"><span></span></a>
+          </div>
 
       </div>
     </div>
